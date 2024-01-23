67f5a1d60cd3bc8ea990ade2e40690009edaba95
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-01-22 22:37:06 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/edu/eci/labinfo/bookinglab/BookinglabApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.labinfo.bookinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinglabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinglabApplication.class, args);
+	}
+
+}
