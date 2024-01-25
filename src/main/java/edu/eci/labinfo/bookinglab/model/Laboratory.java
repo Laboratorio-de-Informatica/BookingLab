c9de290af6289170c5a6bd001380bca7106f2289
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2024-01-24 22:24:53 -0500

    Model

--- src/main/java/edu/eci/labinfo/bookinglab/model/Laboratory.java
+++ src/main/java/edu/eci/labinfo/bookinglab/model/Laboratory.java
@@ -1,15 +1,19 @@
 package edu.eci.labinfo.bookinglab.model;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @EqualsAndHashCode
-
-public abstract class Laboratory {
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Laboratory {
     protected String laboratoryName;
     protected int availableComputers;
-    protected boolean isAvailable;
 }
