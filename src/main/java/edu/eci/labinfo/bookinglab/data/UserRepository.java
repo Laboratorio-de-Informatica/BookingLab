84c3912b5199447486bef4304b33b187274662a7
DavidVal6 <cardoda11@gmail.com>
2024-01-26 07:26:54 -0500

    UserRepository

---
+++ src/main/java/edu/eci/labinfo/bookinglab/data/UserRepository.java
@@ -0,0 +1,16 @@
+package edu.eci.labinfo.bookinglab.data;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.eci.labinfo.bookinglab.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByFullName(String fullName);
+    
+}
