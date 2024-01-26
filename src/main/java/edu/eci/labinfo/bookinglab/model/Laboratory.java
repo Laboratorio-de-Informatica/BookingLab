f82480a8623196ba07336451ed235837aea85193
AndresOnate <andrescamiloquimbayo@gmail.com>
2024-01-26 08:12:09 -0500

    Conexión base de datos #11

--- src/main/java/edu/eci/labinfo/bookinglab/model/Laboratory.java
+++ src/main/java/edu/eci/labinfo/bookinglab/model/Laboratory.java
@@ -1,12 +1,19 @@
 package edu.eci.labinfo.bookinglab.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
+@Entity
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -14,6 +21,11 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Laboratory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long labId;
+
     protected String laboratoryName;
     protected int availableComputers;
 }
